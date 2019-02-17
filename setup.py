0c37d4a18f212655f79f385d5c34e9c5fc54a407
Will Toohey <will@mon.im>
2019-02-17 15:08:08 +1000

    v1.9

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
 if sys.version_info < (3,0):
     requires.append('future')
 
-version = '1.8'
+version = '1.9'
 setup(
     name='ifstools',
     description='Extractor/repacker for Konmai IFS files',
